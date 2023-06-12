6d877a49034cbc4eddef6e93c52c2a97667cb677
leenalzaben <leena.zaben61@gmail.com>
2023-06-12 21:30:34 +0000

    serverless-functions

---
+++ README.md
@@ -0,0 +1,4 @@
+# Lab: Class 16 || capital-finder
+
+[Capital Finder I](http://localhost:3000/api/capital-finder?country=cyprus) <br>
+[Capital Finder II](http://localhost:3000/api/capital-finder?capital=Doha)
--- api/capital-finder.py
+++ api/capital-finder.py
@@ -1,11 +1,40 @@
 from http.server import BaseHTTPRequestHandler
-from datetime import datetime
- 
+from urllib import parse
+import requests
+
 class handler(BaseHTTPRequestHandler):
  
   def do_GET(self):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    
+    api_path = self.path 
+    url_components = parse.urlsplit(api_path)
+    query_list = parse.parse_qsl(url_components.query)
+    my_dictionary = dict(query_list)
+    
+    country = my_dictionary.get('country')
+    capital = my_dictionary.get('capital')
+    display = ''
+
+    if country:
+       url = 'https://restcountries.com/v3.1/name/'
+       country_name = requests.get(url + country)
+       json_file = country_name.json()
+       for country_info in json_file:
+           show = country_info['capital'][0]
+           display = f"The capital of {country} is {show}"
+
+    elif capital:
+        url = 'https://restcountries.com/v3.1/capital/'
+        r = requests.get(url + capital)
+        json_file = r.json()
+        for country_info in json_file:
+            show = country_info['name']['common']
+            display = f"{capital} is the capital of {show}."
+    
+    self.wfile.write(str(display).encode())
+    print(display)
+
     return
